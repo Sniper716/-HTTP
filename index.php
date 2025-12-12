f8e66dc46fd802e96cce3828ce31d267aad995aa
Alexandr <lolipop.xaxax@gmail.com>
2025-12-12 15:32:36 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo '<h1>Задание 2</h1>';
+echo '<ul>';
+echo '<li><a href="404.php">Страница 1 - 404 ошибка</a></li>';
+echo '<li><a href="download.php?text=Пример текста для скачивания">Страница 2 - Скачать файл</a></li>';
+echo '<li><a href="counter.php">Страница 3 - Счетчик</a></li>';
+echo '<li><a href="result.php">Страница 4 - Результат</a></li>';
+echo '</ul>';
